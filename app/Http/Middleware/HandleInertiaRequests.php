66a4be99d991cc6068aa70750e9b4ac690b83b04
Patryk Pilek <patryk.pilek@gmail.com>
2023-04-09 03:52:02 +0100

    create user resource

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Middleware;
 
+use App\Http\Resources\UserResource;
 use Illuminate\Http\Request;
 use Inertia\Middleware;
 use Tightenco\Ziggy\Ziggy;
@@ -31,9 +32,7 @@ class HandleInertiaRequests extends Middleware
     public function share(Request $request): array
     {
         return array_merge(parent::share($request), [
-            'auth.user' => fn () => $request->user() ? $request->user()->only('id', 'name', 'email') : null,
-            'auth.user.roles' => fn () => $request->user() ? $request->user()->getRoleNames() : null,
-            'auth.user.permissions' => fn () => $request->user() ? $request->user()->getPermissionNames() : null,
+            'auth.user' => fn () => $request->user() ? new UserResource($request->user()) : null,
             'ziggy' => function () use ($request) {
                 return array_merge((new Ziggy)->toArray(), [
                     'location' => $request->url(),
