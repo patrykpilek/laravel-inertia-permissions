a84aaa54de56c4b9350c83e8ed23d128a0da153e
Patryk Pilek <patryk.pilek@gmail.com>
2023-04-09 19:32:55 +0100

    display users, roles nad permissions

--- README.md
+++ README.md
@@ -7,3 +7,8 @@
 - Breeze (Dark Mode, VUE)
 - Sail (MySQL, Redis, Mailpit)
 - Spatie - laravel permission
+
+
+### Helpers links
+- [Heroicons](https://heroicons.com/)
+- [Flowbite](https://flowbite.com/docs/getting-started/introduction/)
--- resources/js/Pages/Admin/Permissions/PermissionIndex.vue
+++ resources/js/Pages/Admin/Permissions/PermissionIndex.vue
@@ -1,14 +1,39 @@
 <script setup>
 import AdminLayout from '@/Layouts/AdminLayout.vue';
 import { Head } from '@inertiajs/vue3';
+import Table from "@/Components/Table.vue";
+import TableHeaderCell from "@/Components/TableHeaderCell.vue";
+import TableDataCell from "@/Components/TableDataCell.vue";
+import TableRow from "@/Components/TableRow.vue";
+
+defineProps(['permissions']);
+
 </script>
 
 <template>
     <Head title="Dashboard" />
 
     <AdminLayout>
-        <div class="py-4">
+        <div class="max-w-7xl mx-auto py-4">
             <h1>Permissions Index Page</h1>
+            <div class="mt-6">
+                <Table>
+                    <template #header>
+                        <TableRow>
+                            <TableHeaderCell>ID</TableHeaderCell>
+                            <TableHeaderCell>Name</TableHeaderCell>
+                            <TableHeaderCell>Action</TableHeaderCell>
+                        </TableRow>
+                    </template>
+                    <template #default>
+                        <TableRow v-for="permission in permissions" :key="permission.id" class="border-b">
+                            <TableDataCell>{{ permission.id }}</TableDataCell>
+                            <TableDataCell>{{ permission.name }}</TableDataCell>
+                            <TableDataCell>Edit/Delete</TableDataCell>
+                        </TableRow>
+                    </template>
+                </Table>
+            </div>
         </div>
     </AdminLayout>
 </template>
