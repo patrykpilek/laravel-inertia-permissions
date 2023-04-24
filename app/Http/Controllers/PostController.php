0f968c9aa1523512bf23750f1c547dbc998c15aa
Patryk Pilek <patryk.pilek@gmail.com>
2023-04-24 12:32:51 +0100

    create post CRUD

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,9 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\CreatePostRequest;
 use App\Http\Resources\PostResource;
 use App\Models\Post;
-use Illuminate\Http\Request;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Support\Facades\Redirect;
 use Inertia\Inertia;
 use Inertia\Response;
 
@@ -18,4 +20,45 @@ class PostController extends Controller
             'posts' => PostResource::collection($posts),
         ]);
     }
+
+    public function create(): Response
+    {
+        return Inertia::render('Admin/Posts/Create');
+    }
+
+    public function store(CreatePostRequest $request): RedirectResponse
+    {
+        Post::create($request->validated());
+
+        return to_route('posts.index');
+    }
+
+//    public function edit($id): Response
+//    {
+//        $post = Post::findOrFail($id);
+//
+//        return Inertia::render('Admin/Posts/Edit',  [
+//            'post' => new PostResource($post)
+//        ]);
+//    }
+
+    public function edit(Post $post): Response
+    {
+        return Inertia::render('Admin/Posts/Edit',  [
+            'post' => new PostResource($post)
+        ]);
+    }
+
+    public function update(CreatePostRequest $request, Post $post): RedirectResponse
+    {
+        $post->update($request->validated());
+        return to_route('posts.index');
+    }
+
+    public function destroy(Post $post): RedirectResponse
+    {
+        $post->delete();
+        return back();
+    }
+
 }
